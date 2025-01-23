0d6c7b97d50e3a7d4b68517f69fa75935e10317c
Spencer Warren <113386786+sbw7538@users.noreply.github.com>
2025-01-23 09:07:45 -0500

    user input basic setup

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
-public class Main{
+import java.util.Scanner;
 
-    public static void main(String[] args){
-        System.out.print("Hello World!");
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String input = scanner.next();
+                System.out.println("Input: " + input);
+                String lower = input.toLowerCase();
+                if (lower.startsWith("create")) {
+
+                } else if (lower.startsWith("drop")) {
+
+                } else if (lower.startsWith("alter")) {
+                    
+                } else if (lower.startsWith("insert")) {
+                    
+                } else if (lower.startsWith("display")) {
+                    
+                } else if (lower.startsWith("select")) {
+                    
+                } else {
+                    System.out.println("Invalid input");
+                }
+            }
+        }
     }
-
-
-
 }
