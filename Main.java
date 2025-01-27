829f67cd6104720ac4cfef7187810cd0c9d4bbe5
Sam Ellis <sae1200@rit.edu>
2025-01-27 18:05:15 -0500

    Added basic I/O for display command

--- Main.java
+++ Main.java
@@ -19,6 +19,32 @@ public class Main {
                     
                 } else if (lower.startsWith("display")) {
                     
+                    String nextToken = scanner.next().toLowerCase();
+                    if (nextToken.startsWith("schema")) {
+                        // Display the catalog of the database
+                        // TODO: Check database for catalog info
+                        // Display database location, page size, buffer size, and table schema
+                        System.out.println("=== DISPLAY SCHEMA ===");
+                        System.out.println("DB LOCATION  : ");
+                        System.out.println("PAGE SIZE    : ");
+                        System.out.println("BUFFER SIZE  : ");
+                        System.out.println("TABLE SCHEMA : ");
+
+                    } else if (nextToken.startsWith("info")) {
+                        // 'display info <name>'
+                        String tableName = scanner.next();
+                        // TODO: Check database for table info
+                        // Display table name, schema, number of pages, and number of records
+                        System.out.println("=== DISPLAY INFO ===");
+                        System.out.println("TABLE NAME   : " + tableName);
+                        System.out.println("TABLE SCHEMA : ");
+                        System.out.println("# OF PAGES   : ");
+                        System.out.println("# OF RECORDS : ");
+
+                    } else {
+                        System.out.println("Invalid input, Usage: 'DISPLAY SCHEMA' or 'DISPLAY INFO <name>'");
+                    }
+
                 } else if (lower.startsWith("select")) {
                     
                 } else {
