64d7163bd5fddcb680a0b1f91e6fd06f469e30d9
Sam Ellis <sae1200@rit.edu>
2025-01-30 09:15:27 -0500

    added catalog funcs

--- Catalog.java
+++ Catalog.java
@@ -1,3 +1,12 @@
 public class Catalog {
     // Hello
+
+    public static void readTableSchema(String[] tableName) {
+        // Return record schema for given table name
+    }
+
+    public static void writeTableSchema(String[] tableName, String[] tableSchema) {
+        // Add new table to catalog
+    }
+
 }
