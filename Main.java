32299a7281f554523d5823959319c3cf6e849a11
wolfgrey139 <alexdenny25@gmail.com>
2025-02-06 08:56:29 -0500

    Main Parser functional

--- Main.java
+++ Main.java
@@ -5,11 +5,43 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
+
+            String lower = "";//holds the input string as lowercase
+                              //for the parsers
+
+            while (scanner.hasNext()) {
                 String input = scanner.next();
+
+                if (input.toLowerCase().equals("quit")){
+                    break;
+                }
                 System.out.println("Input: " + input);
-                String lower = input.toLowerCase();
-                if (lower.startsWith("create")) {
+                lower = lower.concat(input.toLowerCase() + " ");
+                System.out.println("LOWER: " + lower);
+                
+                if(lower.strip().endsWith(";")){      //sending user
+                                                             //input to parsers  
+                    if(lower.startsWith("create") ||  
+                        lower.startsWith("drop") || 
+                        lower.startsWith("alter")){
+
+                            //send to DDLParser
+                            System.out.println("DDLParser");
+                            lower = "";
+                    }            
+                    
+                    if(lower.startsWith("insert") ||  
+                        lower.startsWith("display") || 
+                        lower.startsWith("select")){
+
+                            //send to DMLParser
+                            System.out.println("DMLParser");
+                            lower = "";
+
+                    }   
+                }
+                
+                /*if (lower.startsWith("create")) {
 
                 } else if (lower.startsWith("drop")) {
 
@@ -49,7 +81,8 @@ public class Main {
                     
                 } else {
                     System.out.println("Invalid input");
-                }
+                }*/
+
             }
         }
     }
