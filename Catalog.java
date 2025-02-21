5cdb09f363ab0bc5769421efb946aedec5ea754c
Sam Ellis <sae1200@rit.edu>
2025-02-21 02:40:19 -0500

    late night commit, updated catalog class and expanded schema class

--- Catalog.java
+++ Catalog.java
@@ -1,43 +1,43 @@
-import java.io.*;
+// Author: Sam Ellis
+
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import page.RecordEntryType;
 
 public class Catalog {
-    private Map<String, Integer> tableIDs = new HashMap<>();        // [table name -> table ID]
-    private Map<Integer, Schema> tableSchemas = new HashMap<>();    // [table ID -> table schema]
-    private Map<Integer, Integer[]> tablePageIDs = new HashMap<>(); // [table ID -> page ids]
-    private Map<Integer, Integer> tablePageSize = new HashMap<>();  // [table ID -> page size]
-    private String filename; // file location for catalog
+
+    private Map<String, Integer> tableIDs = new HashMap<>();     // [table name -> table ID]
+    private Map<Integer, Schema> tableSchemas = new HashMap<>(); // [table ID -> schema, page size, pageids]
+    private String filename;            // file location for catalog
+    private Integer tableIDCounter = 0; // Internal counter used for assigning table IDs
 
     public Catalog(String filename) {
         this.filename = filename;
     }
 
     public void saveToFile() {
-        // Open a file and write the catalog info as binary data
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-            oos.writeObject(tableIDs);
-            oos.writeObject(tableSchemas);
-            oos.writeObject(tablePageIDs);
-            oos.writeObject(tablePageSize);
-        } catch (IOException e) {
-            e.printStackTrace();
+        int totalBytes = RecordEntryType.INT.size(); // Count number of bytes to allocate (starts with space for 1 int)
+        for (Map.Entry<String, Integer> table : tableIDs.entrySet()) {
+            byte[] tableName = table.getKey().getBytes();
+            byte[] tableSchema = tableSchemas.get(table.getValue()).toBytes();
+            totalBytes += tableName.length + tableSchema.length + RecordEntryType.INT.size(); // Name, schema, ID
         }
+
+        ByteBuffer bbuf = ByteBuffer.allocate(totalBytes+RecordEntryType.INT.size()); // Allocate byte buffer
+        bbuf.putInt(totalBytes); // Store total bytes at start
+        for (Map.Entry<String, Integer> table : tableIDs.entrySet()) {
+            bbuf.put(table.getKey().getBytes()); // table name
+            bbuf.putInt(table.getValue());       // table ID
+            bbuf.put(tableSchemas.get(table.getValue()).toBytes()); // Convert schema object to bytes and insert into buffer
+        }
+
+        // TODO: Actually upload the byte buffer into the file using filename
+        // Also need to add integer to tell size of schema section for every table
     }
 
-    @SuppressWarnings("unchecked")
     public void loadFromFile() {
-        // Open a file and read/set catalog data
-        tableIDs.clear();
-        tableSchemas.clear();
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            tableIDs      = (Map<String, Integer>) ois.readObject();
-            tableSchemas  = (Map<Integer, Schema>) ois.readObject();
-            tablePageIDs  = (Map<Integer, Integer[]>) ois.readObject();
-            tablePageSize = (Map<Integer, Integer>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        // TODO
     }
 
     public Integer getTableID(String tableName) {
@@ -45,35 +45,30 @@ public class Catalog {
     }
 
     public String getTableName(int tableID) {
-        //SEARCH THROUGH THE HASH MAP BCS THERE IS NO BIDIRECTIONAL MAP IN JAVA!
-        for (Map.Entry<String, Integer> entry : tableIDs.entrySet()) {
-            if (tableID == entry.getValue()) {
-                return entry.getKey();
+        // Search through the hash map, could also add a reverse map to link ID -> Name. 
+        for (Map.Entry<String, Integer> table : tableIDs.entrySet()) {
+            if (tableID == table.getValue()) {
+                return table.getKey();
             }
         }
         return null;
     }
 
-    public void addTable(String tableName, int tableID, Schema schema, int pageSize) {
+    public void addTable(String tableName, Schema schema) {
+        int tableID = tableIDCounter;
         tableIDs.put(tableName, tableID);
         tableSchemas.put(tableID, schema);
-        tablePageIDs.put(tableID, new Integer[0]);
-        tablePageSize.put(tableID, pageSize);
+        tableIDCounter += 1;
     }
 
     public void removeTable(int tableID) {
         String tableName = getTableName(tableID);
         tableIDs.remove(tableName);
         tableSchemas.remove(tableID);
-        tablePageIDs.remove(tableID);
-        tablePageSize.remove(tableID);
     }
 
     public void addPage(int tableID) {
-        int numpages = tablePageIDs.get(tableID).length;
-        Integer[] newPageList = new Integer[numpages+1];
-        //TODO: ADD Logic for ordering new page (THIS DOESNT WORK RIGHT NOW)
-        tablePageIDs.put(tableID, newPageList);
+        tableSchemas.get(tableID).addPage();
     }
 
     public Schema getSchema(int tableID) {
--- Schema.java
+++ Schema.java
@@ -1,18 +1,66 @@
-import java.io.Serializable;
+// Author: Sam Ellis
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
+import page.RecordEntryType;
 
-public class Schema implements Serializable {
-    private List<String> attributeTypes; // List of attribute types in order
+public class Schema { // Schema within a particular table
+    private Integer pageSize;                     // Max size of a page in bytes
+    private ArrayList<Integer> pageIDs;           // List of IDs for all pages, in order
+    private List<RecordEntryType> attributeTypes; // List of attribute types, in order
+    private Integer pageIDCount = 0;              // Internal counter, used for assigning IDs to new pages
 
-    public Schema(List<String> attributeTypes) {
+    public Schema(Integer pageSize, ArrayList<Integer> pageIDs, List<RecordEntryType> attributeTypes) {
+        this.pageSize = pageSize;
+        this.pageIDs = pageIDs;
         this.attributeTypes = attributeTypes;
     }
 
-    public List<String> getAttributeTypes() {
+    public Schema(byte[] byteSchema) {
+        // TODO: Initalize schema object from encoded byte array from .toBytes()
+    }
+
+    public void addPage() {
+        pageIDs.add(pageIDCount); //TODO: needs proper logic for ordering
+        pageIDCount += 1;
+    }
+
+    public void removePage(Integer pageID) {
+        pageIDs.remove(pageID); //TODO: needs proper logic for ordering
+    }
+
+    public ArrayList<Integer> getPageIDs() {
+        return pageIDs;
+    }
+
+    public List<RecordEntryType> getAttributeTypes() {
         return attributeTypes;
     }
 
+    public byte[] toBytes() {
+        int totalBytes = (RecordEntryType.INT.size() * 4) +   // pageSize, pageCount, pageIDs size, attrTypes size
+                         (RecordEntryType.INT.size() * pageIDs.size()) + 
+                         (RecordEntryType.INT.size() * attributeTypes.size());
+        ByteBuffer bbuf = ByteBuffer.allocate(totalBytes); // Allocate buffer
+        
+        bbuf.putInt(pageSize);       // Store pageSize
+        bbuf.putInt(pageIDs.size()); 
+        for (Integer id : pageIDs) { // Store pageIDs
+            bbuf.putInt(id);
+        }
+        bbuf.putInt(attributeTypes.size()); 
+        for (RecordEntryType type : attributeTypes) {
+            bbuf.putInt(type.ordinal()); // Store attributeTypes
+        }
+        bbuf.putInt(pageIDCount);   // Store pageIDCount
+
+        return bbuf.array();
+    }
+
     public void printSchema() {
-        System.out.println("Schema: " + String.join(", ", attributeTypes));
+        for (RecordEntryType e : attributeTypes) {
+            System.out.print(e.name());
+        }
     }
 }
