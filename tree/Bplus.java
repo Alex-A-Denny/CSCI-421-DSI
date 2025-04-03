9225f35ab2ce05502a7cf80236352ff0ca9f7cd1
Sam Ellis <sae1200@rit.edu>
2025-04-03 09:01:03 -0400

    added calculate N for bplus

---
+++ tree/Bplus.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class Bplus {
+    /**
+     * Calculate 'N' of the tree, the number of pointers in a node.
+     * @param maxPageSize number of bytes per page
+     * @param maxKeySize max number of bytes per primary key to be indexed
+     * @return
+     */
+    public Integer calculateN(Integer maxPageSize, Integer maxKeySize) {
+        //                                   Add integer for size of pointer
+        return ((int) Math.floor(maxPageSize / (maxKeySize + Integer.SIZE))) - 1;
+    }
+}
